9ae682265f99e4a04dcd7147adea3e6f944b1f36
刘德伟 <liudewei0616@gmail.com>
2024-06-26 15:26:53 +0800

    删除多余的文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,39 +1,10 @@
 <template>
   <view class="index-page">
-    <Hello />
-    <UnoCss />
     <text class="h2"> 查看其它页面示例↓ </text>
-    <view>
-      <navigator v-for="(v, idx) in pages" :key="idx" :url="v.url">{{
-        v.title
-      }}</navigator>
-    </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
-import Hello from '@/components/hello/index.vue'
-import UnoCss from '@/components/unocss/index.vue'
-
-const pages = reactive([
-  {
-    title: 'Pinia Demo',
-    url: '/pages/pinia/index'
-  },
-  {
-    title: 'Axios Demo',
-    url: '/pages/axios/index'
-  },
-  {
-    title: 'uView Demo',
-    url: '/pages/uview/index'
-  },
-  {
-    title: 'UnoCSS Demo',
-    url: '/pages/unocss/index'
-  }
-])
 </script>
 
 <style scoped>
