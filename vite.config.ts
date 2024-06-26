8ad890978259d0f7ac79395c7e7f248e1b9af0bb
刘德伟 <liudewei0616@gmail.com>
2024-06-26 15:56:34 +0800

    添加vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,80 @@
-import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import path from 'path'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import AutoRegistryComponents from 'unplugin-vue-components/vite'
+// import VisualizerPlugin from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    uni(),
+export default defineConfig((): UserConfig => {
+  const isBuild = process.env.NODE_ENV === 'production'
+  const vitePlugins: (PluginOption | PluginOption[])[] = [
+    // 按需加载，自动引入
+    AutoImport({
+      imports: ['vue', 'uni-app', 'pinia'],
+      dts: 'types/auto-imports.d.ts',
+      vueTemplate: true
+    }),
+    // 自动按需引入组件(注意：需注册至 uni 之前，否则不会生效)
+    AutoRegistryComponents({
+      dts: 'types/components.d.ts'
+    }),
     // https://github.com/antfu/unocss
-    Unocss()
-  ],
-  server: {
-    // port: 8080,
-    host: '0.0.0.0',
-    proxy: {
-      '/api/': {
-        target:
-          'https://service-rbji0bev-1256505457.cd.apigw.tencentcs.com/release',
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api/, '')
-      },
-      '/api-prod/': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api-prod/, '')
+    Unocss(),
+    uni()
+  ]
+  if (isBuild) {
+    const buildPlugins: (PluginOption | PluginOption[])[] = [
+      // 打包视图分析
+      // VisualizerPlugin()
+    ]
+    vitePlugins.push(...buildPlugins)
+  }
+  return {
+    plugins: vitePlugins,
+    server: {
+      // port: 8080,
+      host: '0.0.0.0',
+      proxy: {
+        '/interface': {
+          target: 'https://h5.test-mk.net/interface/',
+          devTarget: 'http://15.168.55.40:9000/interface/',
+          targetTest: 'http://15.168.45.123:8091/',
+          changeOrigin: false,
+          secure: false,
+          pathRewrite: {
+            '^/interface': ''
+          }
+        },
+        '/out': {
+          testTarget: 'http://15.168.55.40/interface/',
+          devTarget: 'http://devh5.558888666.com',
+          target: 'http://devh5.558888666.com:8091/',
+          changeOrigin: true,
+          secure: true,
+          pathRewrite: {
+            '^/out': ''
+          }
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components')
-    }
-  },
-  css: {
-    // 配置`scss`和`less`全局变量
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/styles/vars/_base.scss";'
-      },
-      less: {
-        additionalData: '@import "@/styles/vars/_base.less";'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components')
+      }
+    },
+    css: {
+      // 配置`scss`和`less`全局变量
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/styles/vars/_base.scss";'
+        },
+        less: {
+          additionalData: '@import "@/styles/vars/_base.less";'
+        }
       }
     }
   }
